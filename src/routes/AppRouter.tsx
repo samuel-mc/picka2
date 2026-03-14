2b4d3fb0fd7647cd5cca24dc0c3b6301e499f7b0
Samuel Medina <smdoomed@gmail.com>
2026-03-13 21:35:00 -0600

    feat: Implement protected and public routes for authentication flow using React Router.

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -4,14 +4,23 @@ import { UserSignup } from '../pages/Users/UserSignup';
 import { UserLogin } from '../pages/Users/UserLogin';
 import { UserDashboard } from '../pages/Users/UserDashborad';
 // import Dashboard from '../pages/Dashboard';
+import { ProtectedRoute } from './ProtectedRoute';
+import { PublicRoute } from './PublicRoute';
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/user/signup" element={<UserSignup />} />
-        <Route path="/user/login" element={<UserLogin />} />
-        <Route path="/user/dashboard" element={<UserDashboard />} />
+        {/* Rutas Públicas (solo accesibles si NO estás logeado) */}
+        <Route element={<PublicRoute />}>
+          <Route path="/user/login" element={<UserLogin />} />
+        </Route>
+
+        {/* Rutas Protegidas (solo accesibles si ESTÁS logeado) */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/user/signup" element={<UserSignup />} />
+          <Route path="/user/dashboard" element={<UserDashboard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
